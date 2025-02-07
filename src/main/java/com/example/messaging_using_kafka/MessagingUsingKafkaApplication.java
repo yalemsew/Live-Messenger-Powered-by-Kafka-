892199a47b23dc9cd397852ad38a67c47a9ea83f
Yalemsew Mekuriaw <yalemone@gmail.com>
2025-02-06 22:06:57 -0800

    project initlization

---
+++ src/main/java/com/example/messaging_using_kafka/MessagingUsingKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.messaging_using_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingUsingKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingUsingKafkaApplication.class, args);
+    }
+
+}
